408466f4d78e902bbe45d0fdbb97b2918a6a7c13
fmorales <francisco.morales.q@gmail.com>
2019-05-13 19:53:59 -0600

    Ejercicios para sesion 2

--- Homework_2/Tarea_3_2.py
+++ Homework_2/Tarea_3_2.py
@@ -1,4 +1,4 @@
-# Crear un programa que haga lo siguiente
+#1. Crear un programa que haga lo siguiente
 
 principal = []
 num = int(input("Dígame cuántas palabras tiene la lista:"))
@@ -16,3 +16,24 @@ if num1==num:
     print(num1)
 else:
     print('Imposible!!')
+
+#2. Escriba un programa que permita crear una lista de palabras y que, a continuación, pida una palabra y diga cuántas veces aparece esa palabra en la lista
+
+principal = []
+num = int(input("Dígame cuántas palabras tiene la lista:"))
+
+for x in range(1,num+1):
+    print("Dígame la palabra",x)
+    y = input()
+    principal.append(y)
+
+print('La lista creada es:',principal)
+
+buscar = input("Dígame la palabra a buscar:")
+
+conteo = principal.count(buscar)
+
+if conteo>0:
+    print('La palabra',buscar,'aparece',conteo,'veces en la lista.')
+else:
+    print('La palabra',buscar,'no aparece en la lista')
