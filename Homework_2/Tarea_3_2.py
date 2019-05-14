307ed0c2fb900f178ad99926b83817769b79387f
fmorales <francisco.morales.q@gmail.com>
2019-05-13 20:24:35 -0600

    Ejercicios para sesion 2

--- Homework_2/Tarea_3_2.py
+++ Homework_2/Tarea_3_2.py
@@ -37,3 +37,23 @@ if conteo>0:
     print('La palabra',buscar,'aparece',conteo,'veces en la lista.')
 else:
     print('La palabra',buscar,'no aparece en la lista')
+
+#3. Escriba un programa que permita crear una lista de palabras y que, a continuación, pida dos palabras y sustituya la primera por la segunda en la lista
+print('TEST!!!')
+principal = []
+num = int(input("Dígame cuántas palabras tiene la lista:"))
+
+for x in range(1,num+1):
+    print("Dígame la palabra",x)
+    y = input()
+    principal.append(y)
+
+print('La lista creada es:',principal)
+
+num1 = input("Sustituir la palabra:")
+num2 = input("por la palabra:")
+
+for n, i in enumerate(principal):
+    if i == num1:
+        principal[n] = num2
+print('La lista ahora es:',principal)
