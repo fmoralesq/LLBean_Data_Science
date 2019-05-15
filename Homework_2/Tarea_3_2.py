3fd4e468acec96719b295734695393c4241c9e03
fmorales <francisco.morales.q@gmail.com>
2019-05-14 18:51:49 -0600

    Ejercicios para sesion 2

--- Homework_2/Tarea_3_2.py
+++ Homework_2/Tarea_3_2.py
@@ -145,7 +145,7 @@ for x in range(1,num+1):
     y = input()
     principal.append(y)
 
-print('La lista creada es:',principal)
+print('La primera lista creada es:',principal)
 
 num_eli = int(input("Dígame cuántas palabras quiere eliminar:"))
 
@@ -155,4 +155,20 @@ for a in range(1,num_eli+1):
     b = input()
     segunda.append(b)
 
-print('La lista creada es:',principal)
+print('La segunda lista creada es:',segunda)
+
+duplicados = list(set(principal) & set(segunda))
+
+print('Palabras que aparecen en las dos listas:',duplicados)
+
+a = set(principal)
+b = set(segunda)
+c = list(b - a)
+d = list(a - b)
+
+print('Palabras que aparecen en solo lista 1',d)
+print('Palabras que aparecen en solo lista 2',c)
+
+e = list(a+b)
+
+print('Palabras que aparecen en ambas listas ',e)
