d4b814a21b4e554d329fdae43d25d4619261f6f7
fmorales <francisco.morales.q@gmail.com>
2019-06-25 20:57:18 -0600

    Ejercicios para sesion 2

--- Proyecto/Test.py
+++ Proyecto/Test.py
@@ -2,16 +2,29 @@
 import pandas as pd
 
 #open MDP Extract
+from pandas import ExcelWriter
+
 data = pd.read_excel (r'\\CRSJBO-FS\UserData$\fmorales\Desktop\6.14 MDP Extract.xlsx')
 df = pd.DataFrame(data)
 
 #Filter by 1_AllStores
 AllStores = df['Sale Loc:']=='1_AllStores'
 All_S = df[AllStores]
-print(All_S)
+
+#Filter by 2_30k & 15k
+Ks = df['Sale Loc:']=='2_30k & 15k'
+KsK = df[Ks]
+
+#Filter by 1_AllStores
+Exception = df['Sale Loc:']=='3_Exception'
+Exc = df[Exception]
 
 #Create File with filtered values
-All_S.to_excel(r'\\CRSJBO-FS\UserData$\fmorales\Desktop\MDP Extract_Test.xlsx',sheet_name='Sheet1')
+writer = ExcelWriter(r'\\CRSJBO-FS\UserData$\fmorales\Desktop\MDP Extract_Test.xlsx')
+All_S.to_excel(writer,'Sheet1')
+KsK.to_excel(writer,'Sheet2')
+Exc.to_excel(writer,'Sheet3')
+writer.save()
 #Query a usar
 
 
